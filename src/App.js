6a2f044038ef003071296b5d4e70374a62cdc68a
Tom Dugan <thomas@aadwebb.com>
2020-03-06 10:02:40 -0600

    been awhile

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React, { useState } from 'react';
-import Card from './components/card'
+import React, { useState, useEffect } from 'react';
+import Board from './components/board'
 
+initializeDeck from './deck'
 export default function App() {
+  const [cards, setCards] = useState([])
   const [flipped, setFlipped] = useState([])
+
+  useEffect(() => {
+    setCards(initializedDeck())
+  }, [])
 
   const handleClick = (id) => {
     setFlipped([ ...flipped, id ])
@@ -11,14 +17,10 @@ export default function App() {
   return (
     <div>
       <h2>Can you remember where the cards are?</h2>
-      <Card
-        id={1}
-        width={100}
-        height={100}
-        back={`/img/back.png`}
-        front={`/img/react.png`}
-        flipped={flipped.includes(1)}
-        handleClick={() => handleClick(1)}
+      <Board
+        cards={cards}
+        flipped={flipped}
+        handleClick={handleClick}
       />
     </div>
   )
---
+++ src/components/board/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import Card from '../card'
+
+export default function Board({ cards, flipped, handleClick }) {
+  return <div>
+    {
+      cards.map(card => (
+      <Card
+        key={card.id}
+        id={card.id}
+        type={card.type}
+        width={card.width}
+        height={card.height}
+        flipped={flipped.includes(card.id)}
+        handleClick={() => handleClick(card.id)}
+      />
+      ))
+    }
+  </div>
+}
+
+Board.propTypes = {
+  cards: PropTypes.arrayOf(PropTypes.shape({})),
+  flipped: PropTypes.arrayOf(PropTypes.number).isRequired,
+  handleClick: PropTypes.func.isRequired,
+}
